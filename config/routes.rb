a1bc1ee963bd07437c0e83db5fd8bf943b1f2136
rjt103591 <rjt103591@126.com>
2017-11-12 15:41:55 +0800

    Finish homework1

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get '/home',  to: 'static_pages#home'
+
+  get '/index', to: 'users#users'
+
+
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
 end
